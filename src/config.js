8521f6ed99a0db4fa66a3558978e2f24635ca82e
surya.k.kumaresan <surya.k.kumaresan@BDc8-Dx-6PHHH92.dir.svc.accenture.com>
2018-12-21 14:47:08 +0530

    done

--- src/config.js
+++ src/config.js
@@ -1,11 +1,36 @@
 exports.config = {
+
+	// framewoek used
 	framework : 'jasmine',
+
+	// selenium server hosted address
 	seleniumAddress : 'http://localhost:4444/wd/hub',
+
+	// desired capability
 	capabilities : {
-		browserName : 'chrome'
+		'directConnect' : true,
+		'browserName' : 'chrome',
+		'loggingPrefs' : {
+			'driver' : 'WARNING',
+			'server' : 'WARNING',
+			'browser' : 'INFO'
+		}
 	},
+
+	// time interval for every async function
+	allScriptsTimeout : 999999,
+
+	// time interval for every 'it' function
+	jasmineNodeOpts : {
+		defaultTimeoutInterval : 999999
+	},
+
+	// webdriver browser settings
 	onPrepare : function() {
+		browser.manage().window().maximize();
 		browser.driver.manage().timeouts().implicitlyWait(30000);
 	},
-	specs : [ './test/sampleTest2.js' ]
+
+	// supporting specs
+	specs : [ './test/*.js' ]
 }
--- src/test/sampleTest.js
+++ src/test/sampleTest.js
@@ -1,9 +1,44 @@
-describe('Enter GURU99 Name', function() {
-	it('should add a Name as GURU99', function() {
+/*
+ * This jsFile is used to run the number of test cases (i.e, number of 'it' functions) on run.
+ * 
+ * @author: Surya.K
+ */
+describe('Enter AngularJS page\n', function() {
+
+	/*
+	 * This method is used to run @first ->before the start of the 'it'
+	 * 
+	 * @author: Surya.K
+	 */
+	beforeEach(function() {
 		browser.get('https://angularjs.org');
-		element(by.model('yourName')).sendKeys('GURU99');
-		var guru = element(by
-				.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
-		expect(guru.getText()).toEqual('Hello GURU99!');
+	});
+
+	/*
+	 * This method is used to run the test scenario
+	 * 
+	 * @author: Surya.K
+	 */
+	it('1: should check the download button presence', function() {
+		var downloadBtn = element(by
+				.xpath("//a[contains(text(),'Download AngularJS')]"));
+		expect(downloadBtn.isDisplayed());
+		expect(downloadBtn.getText().then(function(text) {
+			text = text.toLowerCase();
+			console.log(text);
+			return text;
+		})).toEqual("download angularjs")
+	});
+
+	/*
+	 * This method is used to run @last ->after the 'it'
+	 * 
+	 * @author: Surya.K
+	 */
+	afterEach(function() {
+		browser.manage().logs().get('browser').then(function(browserLog) {
+			if (browserLog.length > 0)
+				console.log('log: TEST CASE DONE');
+		});
 	});
 });
